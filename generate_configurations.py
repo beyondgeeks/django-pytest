pytest-dev/pytest-django

commit b943a9d76dc8a9295423f693b11d6fb174144fb6

generate_default_envs: fix find_and_add to skip existing variations

.travis.yml
@@ -4,8 +4,6 @@ language: python
 python:
   - "3.4"
 env:
-  - TESTENV=pypy-master-sqlite_file
-  - TESTENV=pypy3-master-sqlite_file
   - TESTENV=python2.6-1.6-sqlite_file
   - TESTENV=python2.7-1.3-sqlite_file
   - TESTENV=python2.7-1.4-sqlite_file
@@ -13,30 +11,18 @@ env:
   - TESTENV=python2.7-1.6-sqlite_file
   - TESTENV=python2.7-1.7-sqlite_file
   - TESTENV=python2.7-1.8-sqlite_file
-  - TESTENV=python2.7-master-mysql_innodb
-  - TESTENV=python2.7-master-mysql_myisam
   - TESTENV=python2.7-master-sqlite_file
-  - TESTENV=python3.2-master-sqlite_file
-  - TESTENV=python3.3-master-sqlite_file
   - TESTENV=python3.4-1.5-sqlite_file
   - TESTENV=python3.4-1.6-sqlite_file
   - TESTENV=python3.4-1.7-sqlite_file
   - TESTENV=python3.4-1.8-sqlite_file
-  - TESTENV=python3.4-master-postgres
   - TESTENV=python3.4-master-sqlite
   - TESTENV=python3.4-master-sqlite_file
   - TESTENV=checkqa-python2.7
   - TESTENV=checkqa-python3.4
 matrix:
   allow_failures:
-    - env: TESTENV=pypy-master-sqlite_file
-    - env: TESTENV=pypy3-master-sqlite_file
-    - env: TESTENV=python2.7-master-mysql_innodb
-    - env: TESTENV=python2.7-master-mysql_myisam
     - env: TESTENV=python2.7-master-sqlite_file
-    - env: TESTENV=python3.2-master-sqlite_file
-    - env: TESTENV=python3.3-master-sqlite_file
-    - env: TESTENV=python3.4-master-postgres
     - env: TESTENV=python3.4-master-sqlite
     - env: TESTENV=python3.4-master-sqlite_file
 install:

generate_configurations.py
@@ -156,6 +156,10 @@ def generate_default_envs(envs):
 
     def find_and_add(variations, env_getter):
         for variation in variations:
+            for existing in result:
+                if env_getter(existing) == variation:
+                    return
+
             for env in reversed(envs):
                 if env_getter(env) == variation:
                     result.add(env)

tox.ini
@@ -1,5 +1,5 @@
 [tox]
-envlist = pypy-master-sqlite_file,pypy3-master-sqlite_file,python2.6-1.6-sqlite_file,python2.7-1.3-sqlite_file,python2.7-1.4-sqlite_file,python2.7-1.5-sqlite_file,python2.7-1.6-sqlite_file,python2.7-1.7-sqlite_file,python2.7-1.8-sqlite_file,python2.7-master-mysql_innodb,python2.7-master-mysql_myisam,python2.7-master-sqlite_file,python3.2-master-sqlite_file,python3.3-master-sqlite_file,python3.4-1.5-sqlite_file,python3.4-1.6-sqlite_file,python3.4-1.7-sqlite_file,python3.4-1.8-sqlite_file,python3.4-master-postgres,python3.4-master-sqlite,python3.4-master-sqlite_file,checkqa-python2.7,checkqa-python3.4
+envlist = python2.6-1.6-sqlite_file,python2.7-1.3-sqlite_file,python2.7-1.4-sqlite_file,python2.7-1.5-sqlite_file,python2.7-1.6-sqlite_file,python2.7-1.7-sqlite_file,python2.7-1.8-sqlite_file,python2.7-master-sqlite_file,python3.4-1.5-sqlite_file,python3.4-1.6-sqlite_file,python3.4-1.7-sqlite_file,python3.4-1.8-sqlite_file,python3.4-master-sqlite,python3.4-master-sqlite_file,checkqa-python2.7,checkqa-python3.4
 
 [testenv]
 whitelist_externals =

beyondgeeks/django-pytest

commit 8baf1f4195cf8f350e1d59196cc4d5722365ac95

Fix typo(?) in b943a9d76dc8a9295423f693b11d6fb174144fb6 which caused some envs not to be included in builds.

.travis.yml
@@ -4,6 +4,8 @@ language: python
 python:
   - "3.4"
 env:
+  - TESTENV=pypy-master-sqlite_file
+  - TESTENV=pypy3-1.8-sqlite_file
   - TESTENV=python2.6-1.6-sqlite_file
   - TESTENV=python2.7-1.3-sqlite_file
   - TESTENV=python2.7-1.4-sqlite_file
@@ -11,18 +13,27 @@ env:
   - TESTENV=python2.7-1.6-sqlite_file
   - TESTENV=python2.7-1.7-sqlite_file
   - TESTENV=python2.7-1.8-sqlite_file
+  - TESTENV=python2.7-master-mysql_innodb
+  - TESTENV=python2.7-master-mysql_myisam
   - TESTENV=python2.7-master-sqlite_file
+  - TESTENV=python3.2-1.6-sqlite_file
+  - TESTENV=python3.3-1.6-sqlite_file
   - TESTENV=python3.4-1.5-sqlite_file
   - TESTENV=python3.4-1.6-sqlite_file
   - TESTENV=python3.4-1.7-sqlite_file
   - TESTENV=python3.4-1.8-sqlite_file
+  - TESTENV=python3.4-master-postgres
   - TESTENV=python3.4-master-sqlite
   - TESTENV=python3.4-master-sqlite_file
   - TESTENV=checkqa-python2.7
   - TESTENV=checkqa-python3.4
 matrix:
   allow_failures:
+    - env: TESTENV=pypy-master-sqlite_file
+    - env: TESTENV=python2.7-master-mysql_innodb
+    - env: TESTENV=python2.7-master-mysql_myisam
     - env: TESTENV=python2.7-master-sqlite_file
+    - env: TESTENV=python3.4-master-postgres
     - env: TESTENV=python3.4-master-sqlite
     - env: TESTENV=python3.4-master-sqlite_file
 install:

generate_configurations.py
@@ -167,7 +167,7 @@ def generate_default_envs(envs):
         for variation in variations:
             for existing in result:
                 if env_getter(existing) == variation:
-                    return
+                    break
 
             for env in reversed(envs):
                 if env_getter(env) == variation:

tox.ini
@@ -1,5 +1,5 @@
 [tox]
-envlist = python2.6-1.6-sqlite_file,python2.7-1.3-sqlite_file,python2.7-1.4-sqlite_file,python2.7-1.5-sqlite_file,python2.7-1.6-sqlite_file,python2.7-1.7-sqlite_file,python2.7-1.8-sqlite_file,python2.7-master-sqlite_file,python3.4-1.5-sqlite_file,python3.4-1.6-sqlite_file,python3.4-1.7-sqlite_file,python3.4-1.8-sqlite_file,python3.4-master-sqlite,python3.4-master-sqlite_file,checkqa-python2.7,checkqa-python3.4
+envlist = pypy-master-sqlite_file,pypy3-1.8-sqlite_file,python2.6-1.6-sqlite_file,python2.7-1.3-sqlite_file,python2.7-1.4-sqlite_file,python2.7-1.5-sqlite_file,python2.7-1.6-sqlite_file,python2.7-1.7-sqlite_file,python2.7-1.8-sqlite_file,python2.7-master-mysql_innodb,python2.7-master-mysql_myisam,python2.7-master-sqlite_file,python3.2-1.6-sqlite_file,python3.3-1.6-sqlite_file,python3.4-1.5-sqlite_file,python3.4-1.6-sqlite_file,python3.4-1.7-sqlite_file,python3.4-1.8-sqlite_file,python3.4-master-postgres,python3.4-master-sqlite,python3.4-master-sqlite_file,checkqa-python2.7,checkqa-python3.4
 
 [testenv]
 whitelist_externals =